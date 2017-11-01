a958fefc5d17c0a4516480ae7841c49659ee15c9
pdvega <pdvegaa@gmail.com>
2017-11-01 17:21:18 +0700

    onChange event

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor(props) {
+    super(props) 
+    this.state = {
+      deadline: 'August 18, 2018',
+      newDeadline: ''
+    }
+  }
+
+  changeDeadline() {
+    this.setState({deadline: 'January 25, 2018'})
+  }
+
   render () {
     return (
-      <div>
-        <div>React countdowntimer</div>
+      <div className='App'>
+        <div className='App-title'>ASEAN 50 years countdown to { this.state.deadline }</div>
         <div>
-          <div>150 Days</div>
-          <div>17 Hours</div>
-          <div>3 Minutes</div>
-          <div>55 Seconds</div>
+          <div className='Clock-days'>150 Days</div>
+          <div className='Clock-hours'>17 Hours</div>
+          <div className='Clock-minutes'>3 Minutes</div>
+          <div className='Clock-seconds'>55 Seconds</div>
         </div>
-        <input placeholder='new date' />
-        <button>Submit</button>
+        <div>
+          <input placeholder='new date' onChange={event => console.log('event', event.target.value)}/>
+          <button onClick={() => this.changeDeadline()}>Submit</button>
+        </div>
       </div>      
     )
   }
